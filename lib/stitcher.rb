483f629740d33cfeb7cd59204be607d669adb6d8
manga_osyo <manga.osyo@gmail.com>
2016-01-12 18:30:52 +0900

    Refactoring.

--- lib/stitcher.rb
+++ lib/stitcher.rb
@@ -3,6 +3,7 @@ require_relative "./stitcher/version"
 require_relative "./stitcher/define_method"
 require_relative "./stitcher/accessor"
 require_relative "./stitcher/stitch"
+require_relative "./stitcher/class_operator"
 
 module Stitcher
 	include Register
@@ -12,8 +13,9 @@ module Stitcher
 
 	refine Module do
 		include Stitcher
-# 		Stitcher.instance_methods.each do |name|
-# 			alias_method name, name
-# 		end
+	end
+
+	refine Class do
+		include ClassOperators
 	end
 end
